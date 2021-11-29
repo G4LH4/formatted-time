04faf1767d457d3ee7f83617133b57bd1d60ab7f
GALHA <totogalha@gmail.com>
2021-11-28 22:33:26 -0300

    README changed

--- src/commands/formatedTime.js
+++ src/commands/formatedTime.js
@@ -4,52 +4,53 @@ import {
   checkExistenceParam,
 } from "../components/handleParameters.js";
 
+import { time, utcTime } from "../date/index.js";
+
 export const getFormattedTime = (format) => {
+  // Check if the format its empty
   checkArgs(format, 1);
+  // Check if the format its a string
   checkTypeString(format);
 
-  const date = new Date();
-  const time = {
-    Y: date.getFullYear(),
-    M: date.getMonth() + 1,
-    D: date.getDate(),
-    d: date.getDay(),
-    H: date.getHours(),
-    m: date.getMinutes(),
-    s: date.getSeconds(),
-    ms: date.getMilliseconds(),
-  };
-
-  const utcTime = {
-    uY: date.getUTCFullYear(),
-    uM: date.getUTCMonth() + 1,
-    uD: date.getUTCDate(),
-    ud: date.getUTCDay(),
-    uH: date.getUTCHours(),
-    um: date.getUTCMinutes(),
-    us: date.getUTCSeconds(),
-    ums: date.getUTCMilliseconds(),
-  };
-
+  //Split the format to get the first word and the second one
   const splitArr = format.split(" ");
+
+  // Check if there is a second word and the first one is "arr"
+  // Convert the format to array
 
   if (splitArr[1] && splitArr[0] === "arr") {
     checkExistenceParam(time, utcTime, splitArr[1]);
 
     const arrFormat = [];
 
+    //
     const options = switchOptions(splitArr[1], time, utcTime);
 
-    const insertDate = arrFormat.push(options);
+    // Insert the data into the array
+    arrFormat.push(options);
 
     return arrFormat;
   }
 
-  checkExistenceParam(time, utcTime, format);
+  // Check if there is a second word and the first one is "str"
+  // Convert the format to string
 
+  if (splitArr[1] && splitArr[0] === "str") {
+    checkExistenceParam(time, utcTime, splitArr[1]);
+
+    const options = switchOptions(splitArr[1], time, utcTime);
+
+    const formatToStr = options.toString();
+
+    return formatToStr;
+  }
+  // Check if the format exist in the obj of dates (../date/index.js)
+  checkExistenceParam(time, utcTime, format);
+  // Return the place of the object where the format exists
   return switchOptions(format, time, utcTime);
 };
 
+// Runs through all the possible cases
 const switchOptions = (format, time, utcTime) => {
   switch (format) {
     case "Y":
--- src/components/handleParameters.js
+++ src/components/handleParameters.js
@@ -1,13 +1,16 @@
+// Checks the length of the param
 export const checkArgs = (params, paramLength) => {
   if (params.length < paramLength) {
     throw new Error("Invalid params length");
   }
 };
 
+// Check if the param its a string
 export const checkTypeString = (params) => {
   if (typeof params !== "string") throw new Error("Invalid type of param");
 };
 
+// Check if the format exist on the params
 export const checkExistenceParam = (param1, param2, format) => {
   if (!param1[format] && !param2[format])
     throw new Error(`Word ${format} isn't on the list`);

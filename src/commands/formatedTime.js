898329b69ffa96839835f19e5ebfa5c69c155b78
GALHA <totogalha@gmail.com>
2021-11-28 21:05:43 -0300

    array option added

--- src/commands/formatedTime.js
+++ src/commands/formatedTime.js
@@ -1,4 +1,8 @@
-import { checkArgs, checkTypeString } from "../componentes/handleParameters.js";
+import {
+  checkArgs,
+  checkTypeString,
+  checkExistenceParam,
+} from "../components/handleParameters.js";
 
 export const getFormattedTime = (format) => {
   checkArgs(format, 1);
@@ -13,11 +17,72 @@ export const getFormattedTime = (format) => {
     H: date.getHours(),
     m: date.getMinutes(),
     s: date.getSeconds(),
+    ms: date.getMilliseconds(),
   };
 
-  const splitMsg = format.split("");
-  const result = splitMsg.map((l) => time[l] ?? l);
-  const joinStr = result.join("");
+  const utcTime = {
+    uY: date.getUTCFullYear(),
+    uM: date.getUTCMonth() + 1,
+    uD: date.getUTCDate(),
+    ud: date.getUTCDay(),
+    uH: date.getUTCHours(),
+    um: date.getUTCMinutes(),
+    us: date.getUTCSeconds(),
+    ums: date.getUTCMilliseconds(),
+  };
 
-  return joinStr;
+  const splitArr = format.split(" ");
+
+  if (splitArr[1] && splitArr[0] === "arr") {
+    checkExistenceParam(time, utcTime, splitArr[1]);
+
+    const arrFormat = [];
+
+    const options = switchOptions(splitArr[1], time, utcTime);
+
+    const insertDate = arrFormat.push(options);
+
+    return arrFormat;
+  }
+
+  checkExistenceParam(time, utcTime, format);
+
+  return switchOptions(format, time, utcTime);
+};
+
+const switchOptions = (format, time, utcTime) => {
+  switch (format) {
+    case "Y":
+      return time["Y"];
+    case "M":
+      return time["M"];
+    case "D":
+      return time["D"];
+    case "d":
+      return time["d"];
+    case "H":
+      return time["H"];
+    case "m":
+      return time["m"];
+    case "s":
+      return time["s"];
+    case "ms":
+      return time["ms"];
+    case "uY":
+      return utcTime["uY"];
+    case "uM":
+      return utcTime["uM"];
+    case "uD":
+      return utcTime["uD"];
+    case "ud":
+      return utcTime["ud"];
+    case "uH":
+      return utcTime["uH"];
+    case "um":
+      return utcTime["um"];
+    case "us":
+      return utcTime["us"];
+    case "ums":
+      return utcTime["ums"];
+  }
 };
--- src/components/handleParameters.js
+++ src/components/handleParameters.js
@@ -7,3 +7,8 @@ export const checkArgs = (params, paramLength) => {
 export const checkTypeString = (params) => {
   if (typeof params !== "string") throw new Error("Invalid type of param");
 };
+
+export const checkExistenceParam = (param1, param2, format) => {
+  if (!param1[format] && !param2[format])
+    throw new Error(`Word ${format} isn't on the list`);
+};

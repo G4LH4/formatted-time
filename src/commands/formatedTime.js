4e1f98a1585c1d403a75be9ae09c7e37543b1fdd
GALHA <totogalha@gmail.com>
2021-11-29 14:08:15 -0300

    package.json changed

--- src/commands/formatedTime.js
+++ src/commands/formatedTime.js
@@ -1,56 +1,19 @@
 import { time, utcTime } from "../date/index.js";
-import { checkExistenceParam } from "../components/handleParameters.js";
+import {
+  checkCamps,
+  checkStringParam,
+  checkOptions,
+} from "../components/handleParameters.js";
 
 export const getFormattedTime = ({ type, format }) => {
-  if (!format || !type) {
-    throw new Error("Both camps are required");
-  }
-  if (typeof type !== "string" || typeof format !== "string") {
-    throw new Error("Only strings are supported");
-  }
+  checkCamps(type, format);
+  checkStringParam(type, format);
 
   return FORMATS[type](format);
 };
 
 const switchOptions = (format, time, utcTime) => {
-  switch (format) {
-    case "Y":
-      return time["Y"];
-    case "M":
-      return time["M"];
-    case "D":
-      return time["D"];
-    case "d":
-      return time["d"];
-    case "H":
-      return time["H"];
-    case "m":
-      return time["m"];
-    case "s":
-      return time["s"];
-    case "ms":
-      return time["ms"];
-    case "uY":
-      return utcTime["uY"];
-    case "uM":
-      return utcTime["uM"];
-    case "uD":
-      return utcTime["uD"];
-    case "ud":
-      return utcTime["ud"];
-    case "uH":
-      return utcTime["uH"];
-    case "um":
-      return utcTime["um"];
-    case "us":
-      return utcTime["us"];
-    case "ums":
-      return utcTime["ums"];
-  }
-};
-
-const checkOptions = (options, format) => {
-  if (options === undefined) throw new Error(`${format} is not supported`);
+  return (format.startsWith("u") ? utcTime : time)[format];
 };
 
 const FORMATS = {

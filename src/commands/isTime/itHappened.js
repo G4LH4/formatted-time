8143aba02a6acc349bb7890d7896da1a90f2ea62
GALHA <totogalha@gmail.com>
2021-12-11 02:09:54 -0300

    itHappend error fixed

--- src/commands/isTime/itHappened.js
+++ src/commands/isTime/itHappened.js
@@ -4,7 +4,12 @@ import { checkInstance } from "../../components/handleParameters.js";
 export const alreadyHappened = (date) => {
   checkInstance(date, Date);
 
+  console.log(date);
   const today = new Date();
 
-  return date > today;
+  if (today < date) {
+    return false;
+  }
+  return true;
+  // return date <= today;
 };

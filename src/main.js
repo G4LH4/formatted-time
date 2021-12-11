306423d12afffafb65c71d694ec35449ad66fbd5
GALHA <totogalha@gmail.com>
2021-12-11 01:56:58 -0300

    exports added to the mainjs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 export { currentTime } from "./commands/currentTime.js";
 export { preciseTime } from "./commands/preciseTime.js";
 export { getCloser } from "./commands/getCloser.js";
+export { daysToWeeks, daysToYear } from "./commands/convertTime/daysConvert.js";
+export { hrToMn, hrToMn, hrToMn } from "./commands/convertTime/hrConvert.js";
+export { mnToMs, mnToHr, mnToSc } from "./commands/convertTime/mnConvert.js";
+export { msToSc, msToMinutes } from "./commands/convertTime/msConvert.js";
+export {
+  yearsConvert,
+  yearsConvert,
+  yearsConvert,
+} from "./commands/convertTime/yearsConvert.js";
+export { isAfter } from "./commands/isTime/isAfter.js";
+export { isBefore } from "./commands/isTime/isBefore.js";
+export { isDate } from "./commands/isTime/isDate.js";
+export { alreadyHappened } from "./commands/isTime/itHappend.js";

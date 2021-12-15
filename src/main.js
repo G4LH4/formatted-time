b6857e98d0b43de99202c3176668e4248227cb4b
GALHA <totogalha@gmail.com>
2021-12-15 19:36:06 -0300

    git reset

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// reset to delete webpack
+
 export { currentTime } from "./commands/currentTime.js";
 export { preciseTime } from "./commands/preciseTime.js";
 export { getCloser } from "./commands/getCloser.js";

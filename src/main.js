c1f3127f45feaab5ac8e45d03a30dc633fb556c0
GALHA <totogalha@gmail.com>
2022-01-12 14:33:27 -0300

    bugs solved

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 export { currentTime } from "./commands/currentTime.js";
 export { preciseTime } from "./commands/preciseTime.js";
-export { getCloser } from "./commands/getCloser.js";
+export { getCloser } from "./commands/getTime/getCloser";
 export { daysToWeeks, daysToYear } from "./commands/convertTime/daysConvert.js";
 export { hrToMn, hrToMs, hrToSc } from "./commands/convertTime/hrConvert.js";
 export { mnToMs, mnToHr, mnToSc } from "./commands/convertTime/mnConvert.js";
@@ -12,7 +12,7 @@ export {
   yrsToCentury,
   yrsToDogYrs,
 } from "./commands/convertTime/yearsConvert.js";
-export { isAfter, isBefore } from "./commands/isTime/isAfter/before.js";
+export { isAfter, isBefore } from "./commands/isTime/isAfter-before";
 export { isDate } from "./commands/isTime/isDate.js";
 export { alreadyHappened } from "./commands/isTime/itHappened.js";
 export { isWeekend } from "./commands/isTime/isWeekend.js";

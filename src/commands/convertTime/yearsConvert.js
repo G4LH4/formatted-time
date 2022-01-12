be6ef5d7e371609efdd49dd7dd5c69687f8632c1
GALHA <totogalha@gmail.com>
2022-01-12 14:50:50 -0300

    I think i solve the error

--- src/commands/convertTime/yearsConvert.js
+++ src/commands/convertTime/yearsConvert.js
@@ -1,4 +1,4 @@
-import { roundTime } from "../../components/index";
+import { roundTime } from "../../components/index.js";
 
 export const yrsToLuster = (yrs) => {
   return roundTime(yrs, yrs / 5);
